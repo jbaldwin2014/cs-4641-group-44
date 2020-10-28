527b33ab718344d4fb3158c46a0070e356133c6f
idiallo78 <55326927+idiallo78@users.noreply.github.com>
2020-10-28 13:24:19 -0400

    Add files via upload

---
+++ Algorithm test/DataCleanup.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import matplotlib
+
+
+# read the data
+df = pd.read_csv('sberbank.csv')
+
+# shape and data types of the data
+print(df.shape)
+print(df.dtypes)
+
+# select numeric columns
+df_numeric = df.select_dtypes(include=[np.number])
+numeric_cols = df_numeric.columns.values
+print(numeric_cols)
+
+# select non numeric columns
+df_non_numeric = df.select_dtypes(exclude=[np.number])
+non_numeric_cols = df_non_numeric.columns.values
+print(non_numeric_cols)
+
+
+
+
