eb726a3e053273175608a5b15b1374308200b48a
Ibrahima <idiallo7@gatech.edu>
2020-10-29 01:19:46 -0400

    deleted empty rows and cols

--- Algorithm test/DataCleanup.py
+++ Algorithm test/DataCleanup.py
@@ -23,31 +23,12 @@ featuresArr = df.columns.values
 print(featuresArr)
 
 
-# Drop rows with any empty cells
-df.dropna(
-    axis=0,
-    how='any',
-    thresh=None,
-    subset=None,
-    inplace=True
-)
-
-
 # drop rows with missing or unknown values.
 # drop rows with missing or unknown values.
 df = df[df.icu_yn != 'Missing']
 df = df[df.icu_yn != 'Unknown']
 df = df[df.icu_yn != 'NA']
 
-# drop rows with missing or unknown values.
-for i in range(len(featuresArr)):
-    ind_missing = df[df[featuresArr[i]] == 'Missing'].index
-    df.drop(ind_missing, axis=0)
-    ind_missing2 = df[df[featuresArr[i]] == 'Unknown'].index
-    df.drop(ind_missing2, axis=0)
-    ind_missing3 = df[df[featuresArr[i]] == 'NA'].index
-    df.drop(ind_missing3, axis=0)
-    print(featuresArr[i])
 
 
 
@@ -56,6 +37,15 @@ cols_to_drop = featuresArr[:3]
 df = df.drop(cols_to_drop, axis=1)
 print(df.columns.values)
 print(df.shape)
+
+# Drop rows with any empty cells
+df.dropna(
+    axis=0,
+    how='any',
+    thresh=None,
+    subset=None,
+    inplace=True
+)
 
 df.to_csv(r'C:\Users\ibrad\PycharmProjects\cleaned_COVID_Data.csv')
 
