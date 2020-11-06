4c52710ea84220c5cbbea2159dace261bc58cbf9
stiwari41 <72278476+stiwari41@users.noreply.github.com>
2020-11-06 12:56:59 -0500

    Update DataCleanup.py

--- Algorithm test/DataCleanup.py
+++ Algorithm test/DataCleanup.py
@@ -35,6 +35,8 @@ df = df[df.medcond_yn != 'Missing']
 df = df[df.medcond_yn != 'Unknown']
 df = df[df.medcond_yn != 'NA']
 df = df[df.current_status != 'Probable Case']
+df = df[df.hosp_yn != 'Missing']
+df = df[df.hosp_yn != 'Unknown']
 
 
 
@@ -65,6 +67,10 @@ df = df.replace(encode_death)
 encode_sex = {"sex": {"Male": 1, "Female": 0}}
 print(encode_sex)
 df = df.replace(encode_sex)
+encode_age_group = {"age_group": {"0 - 9 Years": 0, "10 - 19 Years": 1, "20 - 29 Years": 2, "30 - 39 Years": 3, "40 - 49 Years": 4, "50 - 59 Years": 5, "60 - 69 Years": 6, "70 - 79 Years": 7, "80+ Years": 8}} # dont know how to 1HE this yet
+print(encode_age_group)
+df = df.replace(encode_age_group)
+print(df["age_group"].value_counts())
 
 df.to_csv(r'C:\Users\ibrad\PycharmProjects\cleaned_COVID_Data.csv')
 
