55a9f832c8fc9bb05a4486c6201bd4db8bd2b96b
idiallo78 <55326927+idiallo78@users.noreply.github.com>
2020-11-06 14:56:02 -0500

    updated clustering methods

--- Algorithm test/Kmodes.py
+++ Algorithm test/Kmodes.py
@@ -1,15 +1,31 @@
+import inline
+import matplotlib
 import pandas as pd
 import numpy as np
 import math
 from kmodes.kmodes import KModes
+
+
+# Data viz lib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib
+from matplotlib.pyplot import xticks
+
+
 from sklearn.datasets import make_blobs
 from sklearn.metrics import jaccard_score
-
-
 from yellowbrick.cluster import KElbowVisualizer
+from subprocess import call
+from sklearn import preprocessing
+
+
+
+
 
 
 data = pd.read_csv(r'C:\Users\ibrad\PycharmProjects\cleaned_COVID_Data.csv')
+
 
 
 
@@ -28,95 +44,27 @@ data = pd.read_csv(r'C:\Users\ibrad\PycharmProjects\cleaned_COVID_Data.csv')
 #visualizer.show()        # Finalize and render the figure
 
 
-def jacScorePlot():
-
-    ecArr = []
-    jcArr = []
-    jcXArr = []
-    for i in range(2, 10):
-        km = KModes(n_clusters=i)
-        y = km.fit_predict(X)     #X is not one-hot encoded data, it is normal categorical data
-        tempArrjc = []
-        tempArrec = []
-        tempArrjcX = []
-        for j in range(i):
-            # print(sum(y==j))
-            # print(XX[y==j].mode())
-            jcscore = []
-            ecscore = []
-            jcXscore = []
-            for k in data[y == j].T:
-                try:
-                    # jcscore.append(jaccard_similarity_score(XX.loc[k],XX[y==j].mode().T[0]))
-
-                    ecscore.append(np.linalg.norm(np.array(data.loc[k]) - np.array(data[y == j].mode().T[0])))
-
-                    jcXscore.append(jaccard_score(list(X.loc[k]), list(X[y == j].mode().T[0])))
-                except:
-                    # print(XX.loc[k].T)
-                    # print(XX[y==j].mode())
-                    print(k)
-                    break
-                    # print(np.mean(jcscore))
-                    # tempArrjc.append(np.mean(jcscore))
-                    # tempArrec.append(np.mean(ecscore))
-                tempArrjcX.append(np.mean(jcXscore))
-
-                print("n_cluster =", i, ":", np.mean(tempArrjcX))
-                # jcArr.append(np.mean(tempArrjc))
-                # ecArr.append(np.mean(tempArrec))
-                jcXArr.append(np.mean(tempArrjcX))
 
 
 
+cost = []
+for num_clusters in list(range(1,5)):
+    kmode = KModes(n_clusters=num_clusters, init = "Cao", n_init = 1, verbose=1)
+    kmode.fit_predict(data)
+    cost.append(kmode.cost_)
+
+y = np.array([i for i in range(1,5,1)])
+plt.title('Kmode Elbow Method Modelisation')
+plt.xlabel('k')
+plt.ylabel('Cost')
+plt.plot(y,cost)
+plt.show()
 
 
-
-def makeClusters(data,numClusters):
-    km = KModes(n_clusters=numClusters, init="Cao", n_init=1, verbose=1)
-    #subsetDf = data.loc[data['categoryWant'] == categorLabel].drop(['ICU', 'feature1', 'feature2', 'feature3', 'feature4'], axis=1)
-    #subsetData = subsetDf.values
-    fitClusters = km.fit_predict(data.values)
-    clusterCentroidsDf = pd.DataFrame(km.cluster_centroids_)
-    clusterCentroidsDf.columns = data.columns
-
-    return fitClusters, clusterCentroidsDf
-
-#Similar to the elbow method, we looked at the centroids from each number of clusters adn examine the broadness of the
+#Similar to the elbow method from kmeans, we looked at the centroids from each number of clusters and examine the broadness of the
 #labels found in those clusters to determine the optimal amount of clusters since kmodes does not currently have any general
 #method of finding it as kmeans does.
 
-clusterData1, newData = makeClusters(data, 5)
-
-newData.to_csv(r'C:\Users\ibrad\PycharmProjects\centroid5_COVID_Data.csv')
-
-clusterData2 = makeClusters(data, 10)
-
-newData.to_csv(r'C:\Users\ibrad\PycharmProjects\centroid10_COVID_Data.csv')
-
-
-clusterData3 = makeClusters(data, 25)
-
-newData.to_csv(r'C:\Users\ibrad\PycharmProjects\centroid25_COVID_Data.csv')
-
-clusterData4 = makeClusters(data, 50)
-
-newData.to_csv(r'C:\Users\ibrad\PycharmProjects\centroid50_COVID_Data.csv')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# define the k-modes model
 km = KModes(n_clusters=10, init='Huang', n_init=11, verbose=1)
 # fit the clusters to the skills dataframe
 clusters = km.fit_predict(data)
@@ -135,5 +83,3 @@ for i in range(shape[0]):
         cent = kmodes[i,:]
         for j in data.columns[np.nonzero(cent)]:
             print(j)
-
-
